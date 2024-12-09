eebc1a35ce1b6c759d9e722ec72789013b86926e
Donald Agamah <donald.agamah@ignition633.org>
2024-12-09 10:37:43 +0100

    Improve the startGame function

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
-let firstCard = 6
-let secondCard = 9
+let firstCard = 10
+let secondCard = 4
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
-
+// let sumEl = document.getElementById("sum-el") OR
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
 
 function startGame() {
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
-        message ="Do you want to draw a new card"
+        message = "Do you want to draw a new card"
     } else if (sum === 21) {
         message = "You've got Blackjack!"
-        hasBlackJack = true 
+        hasBlackJack = true
     } else {
         message = "You're out of the game!"
         isAlive = false
     }
-        // if sum > 21 can be neglected since we have already given several dependencies
+    // if sum > 21 can be neglected since we have already given several dependencies
     // } else {
     //     console.log("You're out of the game!")
     // }
-    
+
     // CASH OUT!
     messageEl.textContent = message
+}
+
+function newCard() {
+    let card = 7
+    sum += card
+    renderGame()
 }
