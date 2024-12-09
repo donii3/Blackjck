d4bbdf6ca0b3300cd1c9b0e4ac8b6d82d58ccdbb
Donald Agamah <donald.agamah@ignition633.org>
2024-12-09 14:10:17 +0100

    Implement getRandomNumber function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let firstCard = 10
-let secondCard = 4
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
 let cards = [firstCard, secondCard] // array - ordered list of items
 let sum = firstCard + secondCard
 let hasBlackJack = false
@@ -9,6 +9,18 @@ let messageEl = document.getElementById("message-el")
 // let sumEl = document.getElementById("sum-el") OR
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.querySelector("#cards-el")
+
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 11) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
 
 function startGame() {
     renderGame()
@@ -39,7 +51,7 @@ function renderGame() {
 }
 
 function newCard() {
-    let card = 7
+    let card = getRandomCard()
     sum += card
     cards.push(card)
     renderGame()
