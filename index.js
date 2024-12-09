88f555bd5028b04cc73b72b490ea5cda8820083a
Donald Agamah <donald.agamah@ignition633.org>
2024-12-09 11:29:04 +0100

    Use loop to render cards

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let firstCard = 10
 let secondCard = 4
+let cards = [firstCard, secondCard] // array - ordered list of items
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -14,7 +15,10 @@ function startGame() {
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card"
@@ -37,5 +41,6 @@ function renderGame() {
 function newCard() {
     let card = 7
     sum += card
+    cards.push(card)
     renderGame()
 }
